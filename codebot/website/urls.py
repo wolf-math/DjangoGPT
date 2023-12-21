78cc177d05aa834d095401bbd321aafc7217c651
Aaron Wolf <aaron.wolf@gmail.com>
2023-12-21 10:15:10 +0200

    clean up

--- codebot/website/urls.py
+++ codebot/website/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('history', views.history, name="history"),
-    path('delete_history/<History_id>', views.delete_history, name="delete_history")
+    path(
+        'delete_history/<History_id>',
+        views.delete_history,
+        name="delete_history"
+    )
 ]
